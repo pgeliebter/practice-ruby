f4464ce7c15394c83fefaa454086e015f3d92619
Pinny Geliebter <pgibralter@gmail.com>
2021-06-09 15:14:00 -0400

    Removed reader/writers and used attrs

--- store_item.rb
+++ store_item.rb
@@ -1,12 +1,4 @@
-# item1 = { name: "water", color: "clear", price: "free", brand: "Deer Park" }
-# item2 = { name: "soda", color: "red", price: 2.00, brand: "Cherry Coke Zero" }
-# item3 = { :name => "seltzer", :color => "clear", :price => 1.00, :brand => "Bubly" }
 
-# p "#{item1[:brand]} #{item1[:name]} is #{item1[:price]}."
-# p "#{item1[:brand]} #{item1[:name]} is #{item1[:price]}."
-# p "#{item1[:brand]} #{item1[:name]} is #{item1[:price]}."
-
-# item1 = { name: "water", :goal => "hey" }
 
 
 class Drinks
@@ -21,28 +13,12 @@ class Drinks
     @taxabale = input_taxabale
   end
 
-  def name
-    @name
-  end
-
-  def size
-    @size
-  end
-  
   def price
     if taxabale == true
       @price * 1.0875
     else
       @price
     end
-  end
-
-  def taxabale
-    @taxabale
-  end
-
-  def price=(input_price)
-    @price = input_price
   end
 
   def total
