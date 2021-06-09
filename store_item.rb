f5a7dc7c1ae3b3fe045d64042ba2645814b5ee59
Pinny Geliebter <pgibralter@gmail.com>
2021-06-09 14:41:54 -0400

    Add store_item.rb file

---
+++ store_item.rb
@@ -0,0 +1,17 @@
+# item1 = { name: "water", color: "clear", price: "free", brand: "Deer Park" }
+# item2 = { name: "soda", color: "red", price: 2.00, brand: "Cherry Coke Zero" }
+# item3 = { :name => "seltzer", :color => "clear", :price => 1.00, :brand => "Bubly" }
+
+# p "#{item1[:brand]} #{item1[:name]} is #{item1[:price]}."
+# p "#{item1[:brand]} #{item1[:name]} is #{item1[:price]}."
+# p "#{item1[:brand]} #{item1[:name]} is #{item1[:price]}."
+
+# item1 = { name: "water", :goal => "hey" }
+
+
+class Item
+  
+
+end
+
+
