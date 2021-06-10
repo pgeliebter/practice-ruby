905da380e1dcf813e21d6b6991b088678d28c4ed
Pinny Geliebter <pgibralter@gmail.com>
2021-06-10 14:45:03 -0400

    Adds gives_all_raises and fire_all_employees

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,6 +1,6 @@
-class Transport
+class Vehicle
   attr_reader :speed, :direction
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -19,27 +19,45 @@ class Transport
   end
 end
 
-class Car < Transport
+class Car < Vehicle
+  attr_accessor :make, :model, :year, :color
+  def initialize(input_options)
+    super
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @year = input_options[:year]
+    @color = input_options[:color]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike < Transport
+class Bike < Vehicle
+  attr_accessor :brand, :type
+  def initialize(input_options)
+    super
+    @brand = input_options[:brand]
+    @type = input_options[:type]
+  end
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike = Bike.new
 
-car = Car.new
+car = Car.new(make: "toyota", model: "corolla", year: 2016, color: "black")
+
+bike = Bike.new(brand: "trek", type: "mountain bike")
+
+puts car.make, car.model, car.year, car.color
 
 puts "Your current car speed is #{car.speed}"
 car.accelerate
 puts "Your current car speed is #{car.speed}"
 car.honk_horn
-
+puts ""
 puts "Your current bike speed is #{bike.speed}"
 bike.accelerate
 puts "Your current bike speed is #{bike.speed}"
