16dd289e6faae40c8e358d0d21dcb1237bcff8d3
Pinny Geliebter <pgibralter@gmail.com>
2021-06-10 12:55:04 -0400

    Adds a responsive store clerk

--- store_item.rb
+++ store_item.rb
@@ -28,8 +28,25 @@ end
 drink1 = Drinks.new(size: 20.0, price: 2.00, name: "Cherry Coke Zero", taxable: true)
 drink2 = Drinks.new(size: 16.9, price: 1.0, name: "Water", taxable: false)
 
-drink1.price = 1.75
+drink_array = []
+drink_array << drink1
+drink_array << drink2
 
-drink1.total
+#drink1.price = 1.75
 
+#drink1.total
+
+puts "Here is a list of all our drinks:"
+drink_array.each do |x|
+  puts x.name
+end
+
+puts "Choose one of our drinks by typing it in below:"
+response = gets.chomp.split.map(&:capitalize).join(' ')
+drink_array.each do |x|
+  if response == x.name
+    x.total
+  else
+  end
+end
 
